32bd808b7d5337847261868f20c336f400ba7dd0
Rsiesener <r.siesener91@gmail.com>
2022-01-25 20:37:21 -0600

    Styled the banner to have a gradient

--- src/components/movies/Banner.js
+++ src/components/movies/Banner.js
@@ -1,14 +1,16 @@
 import React, { useState, useEffect } from 'react'
 import styles from '../styles/Banner.module.css'
+
 const pic1 = 'https://images-na.ssl-images-amazon.com/images/I/819cf1ZR2WL.jpg'
+
 const Banner = () => {
-  const [movie, setMovie] = useState('')
   const [showHide, setShowHide] = useState(false)
   const [hovered, setHovered] = useState(false)
   // const data = movie.description
 
-  const info = {
+  const movie = {
     title: 'Dying Hard With More Force Applied.....2!!',
+    image: pic1,
     description:
       'Dying has never been so easy. Yippy Ki Yui Yayy Mother F****R. P.S. Tis The Season For A Great Christmas Movie.Dying has never been so easy. Yippy Ki Yui Yayy Mother F****R. P.S. Tis The Season For A Great Christmas Movie.Dying has never been so easy. Yippy Ki Yui Yayy Mother F****R. P.S. Tis The Season For A Great Christmas Movie.Dying has never been so easy. Yippy Ki Yui Yayy Mother F****R. P.S. Tis The Season For A Great Christmas Movie hi.'
   }
@@ -16,7 +18,6 @@ const Banner = () => {
     // const fetchData = async () => {
     //   const res = await
     // }
-    setMovie(pic1)
   }, [])
 
   const toggle = () => {
@@ -25,27 +26,27 @@ const Banner = () => {
 
   let description = ''
   const cutDescriptionLength = (words) => {
-    words = info.description.slice(0, 100)
+    words = movie.description.slice(0, 100)
     description = words + '...Read More'
     return description
   }
-  cutDescriptionLength(info.description)
+  cutDescriptionLength(movie.description)
 
   return (
-    <div className={styles.banner} style={{ backgroundImage: `url(${movie})` }}>
+    <div className={styles.banner} style={{ backgroundImage: `url(${movie.image})` }}>
       <div className={styles.containerLeft}>
-        <h2 className={styles.title}>{info.title}</h2>
+        <h2 className={styles.title}>{movie.title}</h2>
         <div className={styles.descriptionContainer}>
           <div className={styles.description}
             onMouseEnter={() => setHovered(true)}
             onMouseLeave={() => setHovered(false)}>
-            {hovered ? info.description : description}
+            {hovered ? movie.description : description}
           </div>
           <div className={styles.bannerButtons}>
             <button className={styles.infoBtn} onClick={toggle}>
               Read More
             </button>
-            <button className={styles.infoBtn}>More Info</button>
+            <button className={styles.infoBtn}>Play</button>
           </div>
         </div>
       </div>
