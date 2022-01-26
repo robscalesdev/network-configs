70205759a54837a7ef804e1c2177c9056a26b2cd
rob <imafishyfriend@gmail.com>
2022-01-26 14:20:25 -0600

    added text to make adding descriptions easier

--- src/components/movies/AdminMovie.js
+++ src/components/movies/AdminMovie.js
@@ -55,6 +55,10 @@ const AdminMovie = () => {
           <input type='textarea' name='description' value={movie.description} onChange={handleChange} />
         </label>
       </form>
+      <div>
+        <p>Lorem Ipsum 100 words:</p>
+        <p>tincidunt tortor aliquam nulla facilisi cras fermentum odio eu feugiat pretium nibh ipsum consequat nisl vel pretium lectus quam id leo in vitae turpis massa sed elementum tempus egestas sed sed risus pretium quam vulputate dignissim suspendisse in est ante in nibh mauris cursus mattis molestie a iaculis at erat pellentesque adipiscing commodo elit at imperdiet dui accumsan sit amet nulla facilisi morbi tempus iaculis urna id volutpat lacus laoreet non curabitur gravida arcu ac tortor dignissim convallis aenean et tortor at risus viverra adipiscing at in tellus integer feugiat scelerisque varius morbi enim nunc faucibus a pellentesque sit amet </p>
+      </div>
     </div>
   )
 }
